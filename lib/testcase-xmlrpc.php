97c5e66987b4f597c6df5f4b67617c1331da0979
Max Cutler <max@maxcutler.com>
2012-06-24 10:55:24 -0400

    Added helper function to XML-RPC test base class for making users of a particular role.
    Also fixed including so that all the necessary core APIs get include'd correctly.

--- lib/testcase-xmlrpc.php
+++ lib/testcase-xmlrpc.php
@@ -1,7 +1,7 @@
 <?php
-
-include_once( ABSPATH . WPINC . '/class-IXR.php' );
-include_once( ABSPATH . WPINC . '/class-wp-xmlrpc-server.php' );
+include_once(ABSPATH . 'wp-admin/includes/admin.php');
+include_once(ABSPATH . WPINC . '/class-IXR.php');
+include_once(ABSPATH . WPINC . '/class-wp-xmlrpc-server.php');
 
 class WP_XMLRPC_UnitTestCase extends WP_UnitTestCase {
 	protected $myxmlrpcserver;
@@ -19,5 +19,13 @@ class WP_XMLRPC_UnitTestCase extends WP_UnitTestCase {
 		remove_filter( 'pre_option_enable_xmlrpc', '__return_true' );
 
 		parent::tearDown();
+	}
+
+	protected function make_user_by_role( $role ) {
+		return $this->factory->user->create( array(
+			'user_login' => $role,
+			'user_pass'  => $role,
+			'role'       => $role
+		));
 	}
 }
